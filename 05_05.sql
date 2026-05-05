839f7c8f0625d1d73cbc5469059ca0792acfff8b
Gabriel Fernandes Riguetto <gabrielriguetto3@gmail.com>
2026-05-05 10:53:12 -0300

    Add files via upload

--- 05_05.sql
+++ 05_05.sql
@@ -50,3 +50,38 @@ select * from aproveitamentos
 select dis_codigo, apr_ano, AVG(apr_nota) Media, COUNT(*) qtd from aproveitamentos
 where dis_codigo in (3,7)
 group by dis_codigo, apr_ano
+
+select dis_codigo, apr_ano, AVG(apr_nota) Media, COUNT(*) qtd from aproveitamentos
+group by dis_codigo, apr_ano
+order by dis_codigo, apr_ano
+
+
+select AVG(apr_nota) media
+from aproveitamentos
+group by apr_ano, apr_sem
+
+-- Having --
+select dis_codigo, AVG(apr_nota) Media, COUNT(*) Qtd
+from aproveitamentos
+where apr_nota >= 4
+group by dis_codigo
+having AVG (apr_nota) >= 8 and count (*) > 5
+
+
+----------
+select alu_rm RM, alu_nome Nome, AVG(apr_nota)
+from alunos, aproveitamentos
+ 
+
+select apr_ano, AVG(apr_nota) Media, COUNT(*) qtd from aproveitamentos
+where dis_codigo in (3,7)
+group by dis_codigo, apr_ano
+
+select a.alu_nome Nome, a.alu_rm rm , AVG(apr_nota) Media  
+from  aproveitamentos apr, alunos a
+where apr.apr_ano = 2023 and a.alu_rm = apr.alu_rm and apr.apr_sem = 1
+group by a.alu_rm, a.alu_nome
+having AVG(apr_nota) > 4
+order by Media asc
+
+
